537343abbb831b9bb067de1aea4baf1b96ee7c6f
Ricardo Godoy <rq.godoy@gmail.com>
2019-10-15 19:14:39 -0300

    feat: add create project route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
-const routes = require('./routes');
 
 const server = express();
 
-server.use(routes);
+const projects = [];
+
+server.use(express.json());
+
+server.post('/projects', (req, res) => {
+  const { id } = req.body;
+  const useExists = projects.filter(project => {
+    return project.id === id
+  });
+
+  if (useExists.length) {
+    return res.status(401).json({error: 'Project id already exists'})
+  }
+
+  const project = req.body;
+  project.tasks = [];
+
+  projects.push(project);
+
+  return res.json(req.body);
+});
 
 server.listen(3333);
